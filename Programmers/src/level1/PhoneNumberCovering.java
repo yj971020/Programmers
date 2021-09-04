6df7a7a183c3dd735695a838a8daf98f8b33e477
yj971020 <yj971020@naver.com>
2021-09-04 21:04:25 +0900

    9/4

---
+++ Programmers/src/level1/PhoneNumberCovering.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class PhoneNumberCovering {
+
+	public static void main(String[] args) {
+		String a="01037051938";
+		int i=a.length();
+		System.out.println(i);
+		
+		
+
+	}
+
+}
